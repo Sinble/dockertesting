9fd50853a60529322cbea1201096cdbbf0dc5e85
Nick Bunn <thrain123@gmail.com>
2014-10-20 10:08:38 -0500

    created seperate images for jre and tomcat

--- configureTomcatDockerfile.sh
+++
@@ -1,4 +0,0 @@
-DEFAULT_TOMCAT=7.0.56
-
-sed "s/%VERSION%/${DEFAULT_TOMCAT}/g" Dockerfile.in>Dockerfile
-
---
+++ createImages.sh
@@ -0,0 +1,2 @@
+docker build -t oracle/jre7 oracle/jre/7/ubuntu/
+docker build -t apache/tomcat7 apache/tomcat/7/ubuntu/
