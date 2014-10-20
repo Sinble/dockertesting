cb31849e35a6556e14badad165be0b935dae3685
Nick Bunn <thrain123@gmail.com>
2014-10-20 11:08:47 -0500

    Added centOS and naming standard

--- createImages.sh
+++ createImages.sh
@@ -1,2 +1,4 @@
-docker build -t oracle/jre7 oracle/jre/7/ubuntu/
-docker build -t apache/tomcat7 apache/tomcat/7/ubuntu/
+docker build -t oracle/ub_jre_v7 oracle/jre/7/ubuntu/
+docker build -t openjdk/co_jre_v7 openjdk/jre/7/centos/
+docker build -t apache/ub_tomcat_v7 apache/tomcat/7/ubuntu/
+docker build -t apache/co_tomcat_v7 apache/tomcat/7/centos/
